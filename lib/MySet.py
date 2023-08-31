106f782d2e7bfe96f9753b87a6eba34e872cbbd8
mukoyadariu <mukoya.darius@student.moringaschool.com>
2023-08-31 10:22:44 +0300

    completed assignment

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,26 @@
 class MySet:
+    def __init__(self, initial_values=[]):
+        self.dictionary = {}
+        for value in initial_values:
+            self.dictionary[value] = True
 
-    pass
+    def add(self, value):
+        self.dictionary[value] = True
+
+    def delete(self, value):
+        if value in self.dictionary:
+            del self.dictionary[value]
+
+    def has(self, value):
+        return value in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
+
+# Uncomment these bonus methods if you'd like to implement them
+#     def clear(self):
+#         self.dictionary = {}
+# 
+#     def __str__(self):
+#         elements = ", ".join(str(key) for key in self.dictionary.keys())
+#         return f"MySet: {{{elements}}}"
